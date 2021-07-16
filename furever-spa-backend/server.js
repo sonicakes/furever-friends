0c74a58a47c3943cb984b1a49df976a80508fc38
kmacart <kzmacart@gmail.com>
2021-07-16 15:22:01 +1000

    Commit to show my file structure.

--- furever-spa-backend/server.js
+++ furever-spa-backend/server.js
@@ -8,7 +8,6 @@ const fileUpload = require('express-fileupload')
 
 
 // database connection ----------------------
-console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- furever-spa-frontend/src/components/Signup.vue
+++ furever-spa-frontend/src/components/Signup.vue
@@ -1,4 +1,6 @@
 <template>
+<div>
+    <img src="https://images.unsplash.com/photo-1415369629372-26f2fe60c467?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" height="100vh">
   <div class="page-content page-centered">      
         <div class="signinup-box">
         <img class="signinup-logo" src="../assets/logo.png">
@@ -20,6 +22,7 @@
           </sl-form>
           <p>Have an account? <a href="/signin">Sign In</a></p>
         </div>
+      </div>
       </div>
 </template>
 
