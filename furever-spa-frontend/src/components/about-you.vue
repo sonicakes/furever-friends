d5f9d409bf95295c260eef7ff4f9f2e9205fcb06
Sonia Parker <soniaparker1207@gmail.com>
2021-07-24 16:00:10 +1000

    Forms styling

--- furever-spa-frontend/src/components/about-you.vue
+++ furever-spa-frontend/src/components/about-you.vue
@@ -6,15 +6,15 @@
           <h1>About You</h1>
           <div class="bg-container">
           <sl-form class="form-add-pet" v-on:sl-submit.prevent="addPetSubmitHandler">
-                  <div class="input-group">
-              <sl-select class="custom-input" multiple clearable label="What best describes where you live?" name="preferredLiving" type="text" placeholder="suburban home, apartment, acreage" required>
+                  <div class="input-group custom-input">
+              <sl-select  class="add-pet-input" multiple clearable label="What best describes where you live?" name="preferredLiving" type="text" placeholder="suburban home, apartment, acreage" required>
                 <sl-menu-item value="suburban-home">Suburban Home</sl-menu-item>
                 <sl-menu-item value="apartment">Apartment</sl-menu-item>
                 <sl-menu-item value="acreage">Acreage</sl-menu-item>
               </sl-select>
             </div>
-                      <div class="input-group">
-              <sl-select class="custom-input" multiple clearable label="How large is your yard space?" name="accomodationSize" type="text" placeholder="Non-existent/average size/on the larger side 
+                      <div class="input-group custom-input">
+              <sl-select  class="add-pet-input" multiple clearable label="How large is your yard space?" name="accomodationSize" type="text" placeholder="Non-existent/average size/on the larger side 
 " required>
                 <sl-menu-item value="non-existent">Non-existent</sl-menu-item>
                 <sl-menu-item value="average-size">Average Size</sl-menu-item>
@@ -22,19 +22,19 @@
               </sl-select>
             </div>
             <div class="input-group custom-input">
-              <sl-radio-group label="Select an item">
+              <sl-radio-group label="Select an item" class="add-pet-input">
                 <fieldset part="base" role="radiogroup" class="radio-group">
         <legend part="label" class="radio-group__label">
           <slot name="label">Do you have any other animals in the home?</slot>
         </legend>
         <slot></slot>
       </fieldset>
-  <sl-radio value="yes" checked="">Yes</sl-radio>
+  <sl-radio value="yes">Yes</sl-radio>
   <sl-radio value="no">No</sl-radio>
 </sl-radio-group>
             </div>
-                               <div class="input-group">
-              <sl-select class="custom-input" multiple clearable label="If yes, how would you describe your existing pet?" name="accomodationSize" type="text" placeholder="Non-existent/average size/on the larger side 
+                               <div class="input-group custom-input">
+              <sl-select class="add-pet-input" multiple clearable label="If yes, how would you describe your existing pet?" name="accomodationSize" type="text" placeholder="Non-existent/average size/on the larger side 
 " required>
                 <sl-menu-item value="quiet">Quiet</sl-menu-item>
                 <sl-menu-item value="neutral">Neutral</sl-menu-item>
@@ -49,30 +49,22 @@
         </legend>
         <slot></slot>
       </fieldset>
-  <sl-radio value="yes" checked="">Yes</sl-radio>
+  <sl-radio value="yes">Yes</sl-radio>
   <sl-radio value="no">No</sl-radio>
 </sl-radio-group>
             </div>
-                               <div class="input-group">
-              <sl-select class="custom-input" multiple clearable label="If yes, what is the age of your youngest child?" name="accomodationSize" type="text" placeholder="Children age
+                               <div class="input-group custom-input">
+              <sl-select class="add-pet-input" multiple clearable label="If yes, what is the age of your youngest child?" name="accomodationSize" type="text" placeholder="Children age
 " required>
                 <sl-menu-item value="under-2">under 2</sl-menu-item>
                 <sl-menu-item value="3-6">3-6 yrs old</sl-menu-item>
                 <sl-menu-item value="7-10">7-10 yrs old</sl-menu-item>
               </sl-select>
             </div>
-          
-        
-          
-           
-          
-      
-             
-            <sl-button type="primary" class="submit-btn" submit style="width: 100%;">Submit Your Information</sl-button>
+            <sl-button type="primary" class="add-pet-submit submit-btn" submit>Submit Your Information</sl-button>
           </sl-form>
           </div>
         </div>
-
       </div>
       <blueFooter/>
       </div>
