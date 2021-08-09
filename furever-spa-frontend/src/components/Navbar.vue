f1aeaef2018ddc49146fb7908386f6ab6e093144
Sonia Parker <soniaparker1207@gmail.com>
2021-08-09 16:58:27 +1000

    Remove redundant code in navbar. Changed cog link to go direct to questionnaire form.

--- furever-spa-frontend/src/components/Navbar.vue
+++ furever-spa-frontend/src/components/Navbar.vue
@@ -17,8 +17,8 @@
         <li style="display:inline-block"><img class="navbar-img" src="../assets/logo.png" height="40px" style="margin-top:10px; position: absolute; right: 47vw"><li>
 
 
-        <li class="cogs" style="float:right" onmouseover="function() { $('#cog1').removeClass('paused'); $('#cog1').addClass() }" onmouseout="function() { $('#cog1').addClass('paused'); }">
-          <a class="nav-item top-navbar" exact href="/settings">
+        <li class="cogs" style="float:right">
+          <a class="nav-item top-navbar" href="/about">
             <div class="three-cogs fa-1x">
               <i class="fa fa-cog fa-2x fa-fw spin" id="cog1"></i>
               <i class="fa fa-cog fa-1x fa-fw spin-back" id="cog2"></i>
@@ -27,13 +27,13 @@
         </li>
 
         <li style="float:right">
-          <a href="/profile" class="nav-item top-navbar" exact>
+          <a href="/profile" class="nav-item top-navbar">
             <i class="fas fa-user" style="margin-top: 10px"></i>
           </a>
         </li> 	
 
         <li style="float:right">
-          <a href="/matches" class="nav-item top-navbar" exact id="heart">
+          <a href="/matches" class="nav-item top-navbar" id="heart">
             <i class="fas fa-heart heart" style="margin-top: 10px;scale: 1.25;"></i>
           </a>
         </li>
@@ -72,7 +72,6 @@
 </script>
 
 <style scoped>
-
 .dropdown-toggle::after {
 	content: none;
 }
@@ -113,7 +112,6 @@
     transform: scale( 1.25 );
   }
 }
-
 .cogs:hover .spin, .cogs:hover .spin-back {
   animation-play-state: running;
 }
